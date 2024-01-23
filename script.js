fa38c8a7856841a09cdc512ca583c34ea3bb4b95
HuiNov14 <kiritobuithanhhutluck@gmail.com>
2024-01-23 15:30:33 +0700

    4th commit

--- script.js
+++ script.js
@@ -27,35 +27,107 @@ function clearTable(tableBody) {
 function populateTable(tableBody, results) {
     for (var i = 0; i < results.length; i++) {
         var labels = results[i].labels;
+        var text = results[i].text;
 
         var newRow = tableBody.insertRow();
         var cell1 = newRow.insertCell(0);
         var cell2 = newRow.insertCell(1);
         var cell3 = newRow.insertCell(2);
+        var cell4 = newRow.insertCell(3);
 
         cell1.textContent = i + 1;
-        cell2.textContent = results[i].text;
 
+        // Tạo các phần văn bản với màu sắc tương ứng
+        var coloredText = getColoredText(text, labels);
+        cell2.innerHTML = coloredText;
+
+        // Tạo các hàng nhỏ trong cột Labels
+        for (var j = 0; j < labels.length; j++) {
+            var labelValue = labels[j][2];
+            var labelColor = getTextColor(labelValue);
+
+            var labelRow = cell3.appendChild(document.createElement("div"));
+            labelRow.textContent = labelValue.split("#")[0];
+
+            labelRow.style.color = labelColor;
+        }
+
+        // Tạo các hàng nhỏ trong cột Labels
         for (var j = 0; j < labels.length; j++) {
             var labelValue = labels[j][2];
             var labelColor = getLabelColor(labelValue);
 
-            var labelRow = cell3.appendChild(document.createElement("div"));
-            labelRow.textContent = labelValue.split("#")[0]; 
+            var labelRow = cell4.appendChild(document.createElement("div"));
+            labelRow.textContent = labelValue.split("#")[1];
 
             labelRow.style.color = labelColor;
         }
     }
 }
 
+function getColoredText(text, labels) {
+    // Sắp xếp các nhãn theo vị trí để áp dụng màu sắc cho văn bản
+    labels.sort((a, b) => a[0] - b[0]);
+
+    var coloredText = "";
+    var lastIndex = 0;
+
+    // Tạo các phần văn bản với màu sắc tương ứng
+    for (var i = 0; i < labels.length; i++) {
+        var labelStart = labels[i][0];
+        var labelEnd = labels[i][1];
+        var labelValue = labels[i][2];
+        var labelColor = getTextColor(labelValue);
+
+        // Thêm văn bản trước nhãn
+        coloredText += text.substring(lastIndex, labelStart);
+
+        // Thêm nhãn với màu sắc
+        coloredText += `<span style="color:${labelColor}">${text.substring(labelStart, labelEnd)}</span>`;
+
+        lastIndex = labelEnd;
+    }
+
+    // Thêm phần còn lại của văn bản
+    coloredText += text.substring(lastIndex);
+
+    return coloredText;
+}
+
 function getLabelColor(labelValue) {
     var color;
     if (labelValue.includes("#POSITIVE")) {
-        color = "green";
+        color = "#3772ff";
     } else if (labelValue.includes("#NEGATIVE")) {
-        color = "red";
+        color = "#ff5c8a";
     } else {
-        color = "black";
+        color = "#827081";
+    }
+    return color;
+}
+
+function getTextColor(labelValue) {
+    var color;
+    if (labelValue.includes("SCREEN")) {
+        color = "#52b788";
+    } else if (labelValue.includes("CAMERA")) {
+        color = "#1b4332";
+    } else if (labelValue.includes("FEATURES")) {
+        color = "#74c69d";
+    } else if (labelValue.includes("BATTERY")) {
+        color = "#40916c";
+    } else if (labelValue.includes("PERFORMANCE")) {
+        color = "#ffb600";
+    } else if (labelValue.includes("STORAGE")) {
+        color = "#ff9e00";
+    } else if (labelValue.includes("DESIGN")) {
+        color = "#ff9100";
+    } else if (labelValue.includes("PRICE")) {
+        color = "#ff7900";
+    } else if (labelValue.includes("GENERAL")) {
+        color = "#ff6000";
+    } else if (labelValue.includes("SER&ACC")) {
+        color = "#ff4800";
     }
     return color;
 }
